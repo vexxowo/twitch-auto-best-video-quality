0afe0107ebeb62bf15e594a91391f997c8f8c6f6
vexx <viljoenwickus@gmail.com>
2026-06-09 05:35:24 +0200

    Update twitch-auto-best-video-quality.user.js

--- twitch-auto-best-video-quality.user.js
+++ twitch-auto-best-video-quality.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
-// @name         Twitch - Auto Best Video Quality (VAFT Compatible)
+// @name         Twitch - Auto Best Video Quality (VAFT Compatible & Manual Control)
 // @namespace    https://github.com/vexxowo/
 // @version      1.0.0
-// @description  Automatically forces Twitch to the best available video quality, updated with seamless TwitchAdSolutions (VAFT) compatibility and support for manual quality overrides.
+// @description  Automatically forces Twitch to the best available video quality
 // @author       Martin______X (Modified by vexxowo)
 // @match        https://www.twitch.tv/*
-// @icon         https://www.google.com/s2/favicons?bb=1&domain=twitch.tv
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitch.tv
 // @grant        none
 // @license      MIT
-// @supportURL   https://github.com/vexxowo/twitch-auto-best-video-quality/issues
-// @updateURL    https://raw.githubusercontent.com/vexxowo/twitch-auto-best-video-quality/refs/heads/main/twitch-auto-best-video-quality.user.js
-// @downloadURL  https://raw.githubusercontent.com/vexxowo/twitch-auto-best-video-quality/refs/heads/main/twitch-auto-best-video-quality.user.js
-// ==UserScript==
+// @downloadURL https://raw.githubusercontent.com/vexxowo/twitch-auto-best-video-quality/refs/heads/main/twitch-auto-best-video-quality.user.js
+// @updateURL https://raw.githubusercontent.com/vexxowo/twitch-auto-best-video-quality/refs/heads/main/twitch-auto-best-video-quality.user.js
+// ==/UserScript==
 
 let $url = "";
 let $working = false;
